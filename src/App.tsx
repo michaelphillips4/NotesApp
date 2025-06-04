4eee251f98aa820157650ab62e095703ad0dc0f2
michaelp <michaelp@linux-laptop.lan>
2025-06-04 15:40:09 +0100

    add and list features

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,131 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import { Authenticator } from "@aws-amplify/ui-react";
+import { Amplify } from "aws-amplify";
+import "@aws-amplify/ui-react/styles.css";
+import { getUrl } from "aws-amplify/storage";
+import { generateClient } from "aws-amplify/data";
+import outputs from "../amplify_outputs.json";
+import type { Schema } from "../amplify/data/resource";
 
-function App() {
-  const [count, setCount] = useState(0)
+Amplify.configure(outputs);
+const client = generateClient<Schema>({
+  authMode: "userPool",
+});
 
+type Note = {
+  name: string | null;
+  description: string | null;
+};
+
+function hasRequiredValues(n: Note): unknown {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    n.name !== null &&
+    n.description !== null &&
+    n.name.length > 0 &&
+    n.description.length > 0
+  );
 }
 
-export default App
+export default function App() {
+  const [notes, setNotes] = useState<Note[]>([]);
+  const [name, setName] = useState<string>("");
+  const [description, setDescription] = useState<string>("");
+  const [openSaveDialog, setOpenSaveDialog] = useState<boolean>(false);
+  const [errorMessage, setErrorMessage] = useState<string>("");
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
+  async function fetchNotes() {
+    const { data: notes } = await client.models.Note.list();
+    await Promise.all(
+      notes.map(async (note) => {
+        if (note.image) {
+          const linkToStorageFile = await getUrl({
+            path: ({ identityId }) => `media/${identityId}/${note.image}`,
+          });
+          console.log(linkToStorageFile.url);
+          note.image = linkToStorageFile.url.toString();
+        }
+        return note;
+      })
+    );
+    console.log(notes);
+    setNotes(notes);
+  }
+
+  async function createNote(): Promise<void> {
+    const note: Note = {
+      name,
+      description,
+    };
+    if (hasRequiredValues(note)) {
+      console.log(note);
+      const { data: newNote } = await client.models.Note.create(note);
+
+      console.log("newNote", newNote);
+      setOpenSaveDialog(false);
+      setErrorMessage("");
+      fetchNotes();
+    } else {
+      setErrorMessage("Both a Title and Comment are Required.");
+    }
+  }
+
+  return (
+    <Authenticator>
+      {({ signOut }) => (
+        <>
+          <button className="right" onClick={signOut}>
+            Sign Out
+          </button>
+          <p>
+            Please log comments and bugs below. With bugs please include as much
+            details as possible and ideally include the steps to recreate.
+            Thanks for taking the time to add logs.
+          </p>{" "}
+          <button onClick={() => setOpenSaveDialog(!openSaveDialog)}>
+            Add New Comment/Bug
+          </button>
+          <dialog open={openSaveDialog}>
+            <label htmlFor="noteName">Title</label>
+
+            <input
+              required
+              id="noteName"
+              onInput={(e) => setName(e.currentTarget.value)}
+            />
+            <label htmlFor="noteDescription">Comment</label>
+            <textarea
+              required
+              cols={30}
+              id="noteDescription"
+              onInput={(e) => setDescription(e.currentTarget.value)}
+            />
+
+            <p>{errorMessage}</p>
+
+            <button onClick={createNote}>Save</button>
+
+            <button onClick={() => {
+              setOpenSaveDialog(!openSaveDialog);
+              setErrorMessage("")}}>
+              Close
+            </button>
+          </dialog>
+          <ol>
+            {notes
+              .filter((n) => hasRequiredValues(n))
+              .map((note, i) => (
+                <li key={i}>
+                  <p>
+                    <b>{note.name}</b>:{note.description}
+                  </p>
+                </li>
+              ))}
+          </ol>
+        </>
+      )}
+    </Authenticator>
+  );
+}
